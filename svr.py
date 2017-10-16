5f0dcfb3312bdb9529f544671ae3d22bdd6c5610
alexandre <ale.cuchi.p@gmail.com>
2017-10-15 23:17:25 -0200

    Upload Data. 1.02  - Acertei precisão  - Pegando informação local  - Gerando grafico

--- svr.py
+++ svr.py
@@ -1,107 +1,59 @@
-import math
-
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import quandl
-from sklearn import preprocessing, cross_validation, svm, neural_network
+from sklearn import preprocessing, cross_validation, svm
 
+# variaveis de controle
 
-class DataGet():
-    def __init__(self):
-        self.arr_df = {}
-        self.forecast_out = {}
-        self.forecast_col = {}
-        self.X = self.y = self.x_train = self.y_train = self.x_test = self.y_test = {}
+janela_media = 10
+forecast_col = "Adj. Close"
 
-    def get_data(self, arr_company, start_date='2016-01-01', end_date='2016-31-12'):
-        # Pegando informação do site
-        for i in arr_company:
-            self.arr_df[arr_company] = quandl.get(i, start_date=start_date, end_date=end_date)
+valor_grafico = 50
+df_google = quandl.get("WIKI/GOOGL", start_date="20090101")
 
-    def calcula_percentual(self, col1, col2):
-
-        self.arr_df["WIKI/GOOGL"]["HL_PRC"] = (self.arr_df["WIKI/GOOGL"]["Adj. High"] -
-                                               self.arr_df["WIKI/GOOGL"]["Adj. Close"]) / \
-                                              self.arr_df["WIKI/GOOGL"]["Adj. Close"] * 100
-        self.arr_df["WIKI/GOOGL"]["PRC_change"] = (self.arr_df["WIKI/GOOGL"]["Adj. Close"] -
-                                                   self.arr_df["WIKI/GOOGL"]["Adj. Open"]) / \
-                                                  self.arr_df["WIKI/GOOGL"]["Adj. Open"] * 100
-        self.arr_df["WIKI/GOOGL"] = self.arr_df["WIKI/GOOGL"][["Adj. Close", "HL_PRC", "PRC_change", "Adj. Volume"]]
-
-    '''
-    PArametro Array no formato [[Codigo_compania1,[campo1,campo2...]],
-                                [Codigo_compania2,[campo1,campo2...]]
-    '''
-
-    def set_data_config(self, arr_campos):
-        self.forecast_col = "Adj. Close"
-        for _ in arr_campos:
-            self.arr_df[_].fillna(-99999, inplace=True)
-            self.forecast_out = int(math.ceil(0.01 * len(self.arr_df[_])))
-            self.arr_df[_]["label"] = self.arr_df[_][self.forecast_col].shift(-self.forecast_out)
-            self.arr_df[_].dropna(inplace=True)
-
-#_----------------------------------------------------------
-
-    def separa_treino(self):
-
-        for _ in self.arr_df:
-            self.X[_] = np.array(self.arr_df[_].drop(["label"], 1))
-            self.X[_] = preprocessing.scale(self.X)
-            self.y[_] = np.array(self.arr_df[_]["label"])
-            self.y[_] = np.array(self.arr_df[_]["label"])
-            self.x_train[_], self.x_test[_], self.y_train[_], self.y_test[_] = \
-                cross_validation.train_test_split(self.X[_], self.y[_], test_size=0.2)
-
-
-# df_google = quandl.get("WIKI/GOOGL",start_date='2016-01-01',end_date='2017-01-01')
-
-df_google = pd.DataFrame(np.load("/home/alexandre/Documents/file.npy"))
+# df_google = pd.DataFrame(np.load("/home/alexandre/Documents/file.npy"))
 
 df_google = df_google[["Adj. Open", "Adj. High", "Adj. Low", "Adj. Close", "Adj. Volume"]]
 
 df_google["Perc_High"] = (df_google["Adj. High"] - df_google["Adj. Close"]) / df_google["Adj. Close"] * 100
 df_google["Pert_Low"] = (df_google["Adj. Close"] - df_google["Adj. Open"]) / df_google["Adj. Open"] * 100
-df_google["media_10"] = df_google["Adj. Close"].rolling(window=10).mean()
-df_google.drop(df_google.index[range(10)])
+df_google["media_10"] = df_google["Adj. Close"].rolling(window=janela_media).mean()
+# df_google.drop(df_google.index[range(janela_media)])
 
 df_google = df_google[["Adj. Close", "Perc_High", "Pert_Low", "media_10", "Adj. Volume"]]
 
-forecast_col = "Adj. Close"
-forecast_out = int(math.ceil(0.0001 * len(df_google)))
-df_google["label"] = df_google[forecast_col].shift(-forecast_out)
+forecast_out = 40  # int(math.ceil(0.001*len(df_google)))
 
-df_google.fillna(-99999, inplace=True)
-df_google.dropna(inplace=True)
+df_google["label"] = df_google[forecast_col].shift(forecast_out)
+
+df_google = df_google[["Adj. Close", "Perc_High", "Pert_Low", "media_10", "Adj. Volume", "label"]]
+df_google = df_google[forecast_out if forecast_out > janela_media else janela_media:]
+# df_google.fillna(-99999, inplace=True)
+# df_google.dropna(inplace=True)
 
 X = np.array(df_google.drop(["label"], 1))
-y = np.array(df_google["label"])
-X = preprocessing.scale(X)
+X_A = preprocessing.scale(X)
+
 y = np.array(df_google["label"])
 
-Valor_treino, Valor_teste, Resposta_treino, Resposta_teste = cross_validation.train_test_split(X, y, test_size=0.2)
+Valor_treino, Valor_teste, Resposta_treino, Resposta_teste = cross_validation.train_test_split(X_A, y, test_size=0.2)
+# tamanho_teste = math.ceil(0.2*len(X_A))
+# Valor_treino, Valor_teste = X_A[:-tamanho_teste], X_A[-tamanho_teste:]
+# Resposta_treino, Resposta_teste = y[:-tamanho_teste], y[:-tamanho_teste]
 
 clf_svr = svm.SVR(kernel="linear")
-clf_neuralnet = neural_network.MLPRegressor()
 clf_svr.fit(Valor_treino, Resposta_treino)
-clf_neuralnet.fit(Valor_treino, Resposta_treino)
-# prediction = clf.predict(Valor_teste)
-accuracySVM = clf_svr.predict(Valor_teste)
+
+# print(clf_neuralnet.score(, np.array([1007.87])))
+accuracySVM = clf_svr.score(Valor_teste[-forecast_out:], Resposta_teste[-forecast_out:])
+predict = clf_svr.predict(Valor_teste[-forecast_out:])
 # accuracyNN = clf_neuralnet.predict(Valor_teste)
-
-
-
-# grafico = pd.DataFrame(accuracySVM).tail(60)
-# grafico.cumsum()
-# plt.figure(); grafico.plot(); plt.legend(loc="best")
-valor_grafico = 300
-plt.plot(range(len(accuracySVM[:valor_grafico])), accuracySVM[:valor_grafico], '',
-         range(len(Resposta_teste[:valor_grafico])), Resposta_teste[:valor_grafico], '')
+print(predict, accuracySVM, Resposta_teste[-forecast_out:], sep='\n')
+print(df_google.tail())
+forecast_out = 40
+plt.plot([i for i in range(forecast_out)], predict[-forecast_out:], 'red',
+         [j for j in range(forecast_out)], Resposta_teste[-forecast_out:], 'blue')
 plt.show()
-
-print("mostro")
-
 # print(prediction)
 
 
